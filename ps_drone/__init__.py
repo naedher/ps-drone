94410f44c15d350eb145a6557113a142ad01fd37
naedher <166296413+naedher@users.noreply.github.com>
2025-01-09 18:19:27 +0100

    Update __init__.py

--- ps_drone/__init__.py
+++ ps_drone/__init__.py
@@ -1,9 +1,10 @@
-import ps_drone2
-import ps_drone3
+
 
 import sys
 
 if sys.version_info[0] < 3:
-    ps_drone = ps_drone2
+    import ps_drone2 as ps_drone
 else:
-    ps_drone = ps_drone3
+    import ps_drone3 as ps_drone
+
+    
